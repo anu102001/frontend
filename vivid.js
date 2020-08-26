7df8de6d5a34ffc6cdd4a6f2cdb17235bcbff2c6
ThePrakharRajpali <rprakhar@iitg.ac.in>
2020-08-26 19:10:46 +0530

    Fixed toggle button animation

--- vivid.js
+++ vivid.js
@@ -3,6 +3,7 @@ var body = document.querySelector("body");
 var thumbnail = document.querySelectorAll("thumbnail");
 
 toggleButton.addEventListener("click",() => {
+    toggleButton.classList.toggle('active');
     body.classList.toggle('active');
     for(var i=0; i<thumbnail.length; i++){
         thumbnail[i].classList.toggle('active');
