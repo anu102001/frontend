c2e9c884dc37019a407fccf4e1514683e4dbcf00
ThePrakharRajpali <rprakhar@iitg.ac.in>
2020-08-28 00:05:10 +0530

    Add light mode transitions (basic)

--- vivid.js
+++ vivid.js
@@ -1,11 +1,12 @@
 var toggleButton = document.querySelector(".toggle-btn");
 var body = document.querySelector("body");
 var thumbnail = document.querySelectorAll("thumbnail");
+var bigBox = document.querySelector(".big-box");
+var smallBox = document.querySelector(".small-box");
 
 toggleButton.addEventListener("click",() => {
     toggleButton.classList.toggle('active');
     body.classList.toggle('active');
-    for(var i=0; i<thumbnail.length; i++){
-        thumbnail[i].classList.toggle('active');
-    }
+    bigBox.classList.toggle('active');
+    smallBox.classList.toggle('active');
 });
